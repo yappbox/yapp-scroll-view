29e01530ce786b57017934f2b82cb6436b5c30b0
Luke Melia <luke@lukemelia.com>
2020-04-11 15:34:11 +0800

    Remove unused computed-macros

--- addon/templates/components/computed-macros.js
+++
@@ -1,7 +0,0 @@
-import { computed, get } from '@ember/object';
-
-export function notEqual(dependentKey, value) {
-  return computed(dependentKey, function() {
-    return get(this, dependentKey) !== value;
-  });
-}
